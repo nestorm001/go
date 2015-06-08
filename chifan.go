d8ff3a24c8f4e22741305b3056142754ca65491d
nesto <nestorm1@163.com>
2015-06-08 15:40:47 +0800

    I don't know what the fuck code I wrote

--- chifan.go
+++ chifan.go
@@ -3,9 +3,15 @@
 import (
     "time"
     "math/rand"
+	"os/exec"
+	// "os"
+	// "fmt"
 )
 
 func main() {
 	shop := []string{"黄焖鸡", "麻辣烫", "石锅拌饭", "拉面", "外卖"}
-	print(shop[rand.New(rand.NewSource(time.Now().UnixNano())).Intn(len(shop))])    
+	print(shop[rand.New(rand.NewSource(time.Now().UnixNano())).Intn(len(shop))])
+	//莫要瞎搞
+    // cmd := exec.Command("cmd.exe", "/c", "shutdown -l")
+    // cmd.Run()
 }
