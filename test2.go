0b2805277986ae9e8f188b46bfc1dfb093027f20
nesto <nestorm1@163.com>
2015-06-11 17:03:37 +0800

    gogogo

---
+++ test2.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func sum(a []int, c chan int) {
+	sum := 0
+	for _, v := range a {
+		sum += v
+	}
+	c <- sum // send sum to c
+}
+
+func main() {
+	const num = 1e9
+	a := [num]int{}
+	i := 0
+	for i < num {
+		a[i] = i
+		i++
+	}
+
+	c := make(chan int)
+	go sum(a[:len(a)/4], c)
+	go sum(a[len(a)/4:len(a)/2], c)
+	go sum(a[len(a)/2:len(a)*3/4], c)
+	go sum(a[len(a)*3/4:], c)
+	v1, v2, v3, v4 := <-c, <-c, <-c, <-c // receive from c
+
+	fmt.Println(v1 + v2 + v3 + v4)
+}
