f2c3499a8b019a4b1e2bce64a71410ae3c768215
nesto <nestorm1@163.com>
2015-07-30 15:13:50 +0800

    chifan web

--- web_study/chifan_web.go
+++ web_study/chifan_web.go
@@ -2,71 +2,37 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"math/rand"
 	"net/http"
 	"time"
-	"strconv"
 	"io"
-	"crypto/md5"
 )
 
 var num int16
-var slice []string
+var output string
 
-func chifan(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("method:", r.Method) //获取请求的方法
+func cf(w http.ResponseWriter, r *http.Request) {
+//	fmt.Println("method:", r.Method) //获取请求的方法else {
+
+	shop := []string{"黄焖鸡", "麻辣烫", "石锅拌饭", "拉面", "外卖"}
+	result := shop[rand.New(rand.NewSource(time.Now().UnixNano())).Intn(len(shop))]
 	if r.Method == "GET" {
-		t, _ := template.ParseFiles("web_study\\generate.html")
-		t.Execute(w, nil)
+		output = ""
 	} else {
-		r.ParseForm() //解析参数，默认是不会解析的
 		num++
+		output = "<pre>Hello there!\n今天吃" + result + "吧！\n</pre>"
 		fmt.Printf("被访问了%d次\n", num)
-		shop := []string{"黄焖鸡", "麻辣烫", "石锅拌饭", "拉面", "外卖"}
-		result := shop[rand.New(rand.NewSource(time.Now().UnixNano())).Intn(len(shop))]
-		//		fmt.Fprintf(w, "Hello there!\n") //这个写入到w的是输出到客户端的
-		//		fmt.Fprintf(w, "今天吃" + result + "吧！\n")
-				template.HTMLEscape(w, []byte("Hello there!\n今天吃" + result + "吧！\n"))
-//		r.Form.Set("submit", result)
 		fmt.Println(result)
-		//		r.Form.Set("result", "Hello there!\n今天吃" + result + "吧！\n")
 	}
-}
+	r.ParseForm() //解析参数，默认是不会解析的
 
-func login(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("method:", r.Method) //获取请求的方法
-	if r.Method == "GET" {
-		crutime := time.Now().Unix()
-		h := md5.New()
-		io.WriteString(h, strconv.FormatInt(crutime, 10))
-		token := fmt.Sprintf("%x", h.Sum(nil))
-
-		t, _ := template.ParseFiles("web_study\\login.html")
-		t.Execute(w, token)
-	} else {
-		//请求的是登陆数据，那么执行登陆的逻辑判断
-		r.ParseForm()
-		token := r.Form.Get("token")
-		if token != "" {
-			//验证token的合法性
-		} else {
-			//不存在token报错
-		}
-		if len(r.Form["username"][0]) == 0 || len(r.Form["password"][0]) == 0 {
-			fmt.Fprintf(w, "用户名或密码不能为空！")
-		}
-		fmt.Println("username length:", len(r.Form["username"][0]))
-		fmt.Println("username:", template.HTMLEscapeString(r.Form.Get("username"))) //输出到服务器端
-		fmt.Println("password:", template.HTMLEscapeString(r.Form.Get("password")))
-		template.HTMLEscape(w, []byte(r.Form.Get("username"))) //输出到客户端
-	}
+	io.WriteString(w, "<html><head><meta http-equiv=\"Content-Type\"content=\"text/html; charset=utf-8\"/><title>今天吃什么？</title></head>" +
+	"<body><form action=\"/\" method=\"post\"><input type=\"submit\" name=\"result\" value=\"随机一下\"></form>" + output + "</body></html>")
 }
 
 func main() {
-	http.HandleFunc("/", chifan) //设置访问的路由
-	http.HandleFunc("/login", login)
+	http.HandleFunc("/", cf) //设置访问的路由
 	err := http.ListenAndServe(":8888", nil) //设置监听的端口
 	num = 0
 	if err != nil {
--- web_study/web.go
+++ web_study/web.go
@@ -2,31 +2,75 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
-	"net/http"
-	"strings"
 	"math/rand"
+	"net/http"
 	"time"
+	"strconv"
+	"io"
+	"crypto/md5"
 )
 
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()       //解析参数，默认是不会解析的
-	fmt.Println(r.Form) //这些信息是输出到服务器端的打印信息
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
+var num int16
+var slice []string
+
+func chifan(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method) //获取请求的方法
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("web_study\\generate.html")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm() //解析参数，默认是不会解析的
+		num++
+		fmt.Printf("被访问了%d次\n", num)
+		shop := []string{"黄焖鸡", "麻辣烫", "石锅拌饭", "拉面", "外卖"}
+		result := shop[rand.New(rand.NewSource(time.Now().UnixNano())).Intn(len(shop))]
+		//		fmt.Fprintf(w, "Hello there!\n") //这个写入到w的是输出到客户端的
+		//		fmt.Fprintf(w, "今天吃" + result + "吧！\n")
+		template.HTMLEscape(w, []byte("Hello there!\n今天吃" + result + "吧！\n"))
+		//		t, _ := template.ParseFiles("web_study\\generate.html")
+		//		t.Execute(w, nil)
+		//		r.Form.Set("submit", result)
+		fmt.Println(result)
+		//		r.Form.Set("result", "Hello there!\n今天吃" + result + "吧！\n")
 	}
-	fmt.Fprintf(w, "Hello there!\n") //这个写入到w的是输出到客户端的
-	shop := []string{"黄焖鸡", "麻辣烫", "石锅拌饭", "拉面", "外卖"}
-	fmt.Fprintf(w, "今天吃" + shop[rand.New(rand.NewSource(time.Now().UnixNano())).Intn(len(shop))] + "吧！\n")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method) //获取请求的方法
+	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		t, _ := template.ParseFiles("web_study\\login.html")
+		t.Execute(w, token)
+	} else {
+		//请求的是登陆数据，那么执行登陆的逻辑判断
+		r.ParseForm()
+		token := r.Form.Get("token")
+		if token != "" {
+			//验证token的合法性
+		} else {
+			//不存在token报错
+		}
+		if len(r.Form["username"][0]) == 0 || len(r.Form["password"][0]) == 0 {
+			fmt.Fprintf(w, "用户名或密码不能为空！")
+		}
+		fmt.Println("username length:", len(r.Form["username"][0]))
+		fmt.Println("username:", template.HTMLEscapeString(r.Form.Get("username"))) //输出到服务器端
+		fmt.Println("password:", template.HTMLEscapeString(r.Form.Get("password")))
+		template.HTMLEscape(w, []byte(r.Form.Get("username"))) //输出到客户端
+	}
 }
 
 func main() {
-	http.HandleFunc("/", sayhelloName)       //设置访问的路由
-	err := http.ListenAndServe(":9090", nil) //设置监听的端口
+	http.HandleFunc("/", chifan) //设置访问的路由
+	http.HandleFunc("/login", login)
+	err := http.ListenAndServe(":8888", nil) //设置监听的端口
+	num = 0
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
