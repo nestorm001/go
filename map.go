025996d64b71a176de0ec27fe97b8583e185875a
nesto <nestorm1@163.com>
2015-08-12 15:59:40 +0800

    go on learning

--- map.go
+++ map.go
@@ -23,4 +23,38 @@ func main() {
 	for country := range countryCapitalMap {
 		fmt.Println("Capital of", country, "is", countryCapitalMap[country])
 	}
+
+	capitals := map[string]string{"France":"Paris", "Italy":"Rome", "Japan":"Tokyo"}
+	for key := range capitals {
+		fmt.Println("Map item: Capital of", key, "is", capitals[key])
+	}
+
+	days := map[string]string{"1":"Monday",
+		"2":"Tuesday",
+		"3":"Wednesday",
+		"4":"Thursday",
+		"5":"Friday",
+		"6":"Saturday",
+		"7":"Sunday"}
+
+	var hasTuesday, hasHollyday bool
+	for _, day := range days {
+		fmt.Println(day)
+		if day == "Tuesday" {
+			hasTuesday = true
+		}
+		if day == "Hollyday" {
+			hasHollyday = true
+		}
+	}
+	if hasTuesday {
+		fmt.Println("There is a Tuesday")
+	} else {
+		fmt.Println("There is not a Tuesday")
+	}
+	if hasHollyday {
+		fmt.Println("There is a Hollyday")
+	} else {
+		fmt.Println("There is not a Hollyday")
+	}
 }
