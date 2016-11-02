db931cc1f418ba7a0afc4430cb56817eb13943d4
nesto <nestorm1@163.com>
2016-11-02 15:57:06 +0800

    watchman3

---
+++ traps/watchman3.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	for _, name := range []string{
+		"BeagleBoy",
+		"Huey",
+		"Dewey",
+		"Louie",
+		"Mickey",
+	} {
+		//if check(name) {
+		if realCheck(name) {
+			fmt.Println("Hello", name)
+		} else {
+			fmt.Println(name, ", you shall not pass!")
+		}
+	}
+}
+
+// Returns true if name is a legit nephew
+func check(name string) bool {
+	switch name {
+	case "Huey":
+	case "Dewey":
+	case "Louie":
+		return true
+	}
+	return false
+}
+
+func realCheck(name string) bool {
+	switch name {
+	case "Huey", "Dewey", "Louie":
+		return true
+	}
+	return false
+}
